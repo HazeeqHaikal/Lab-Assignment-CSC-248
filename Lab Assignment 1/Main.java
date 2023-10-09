ce8a5134fa4e9e565bb811ce432df136835786f8
HazeeqHaikal <haikalroslan740@gmail.com>
2023-10-10 00:49:33 +0800

    sync

--- Lab Assignment 1/Main.java
+++ Lab Assignment 1/Main.java
@@ -8,19 +8,25 @@ public class Main {
         Scanner strInput = new Scanner(System.in);
         Scanner intInput = new Scanner(System.in);
 
-        ArrayList<Land> lands = new ArrayList<Land>();
+        // ArrayList<Land> lands = new ArrayList<Land>();
+        int size = intInput.nextInt();
+        Land[] lands = new Land[size];
 
         // read customerData.txt
         try {
             BufferedReader br = new BufferedReader(new FileReader("customerData.txt"));
             String line = br.readLine();
+            int count = 0;
             while (line != null) {
                 String id = line.split(";")[0];
                 String ownerName = line.split(";")[1];
                 char houseType = line.split(";")[2].charAt(0);
                 double area = Double.parseDouble(line.split(";")[3]);
 
-                lands.add(new Land(id, ownerName, houseType, area));
+                // lands.add(new Land(id, ownerName, houseType, area));
+                lands[count] = new Land(id, ownerName, houseType, area);
+                count++;
+
                 line = br.readLine();
             }
 
@@ -46,77 +52,139 @@ public class Main {
 
             // sort based on the tax price and display the list
             // O(n^2)
-            for (int i = 0; i < lands.size() - 1; i++) {
-                for (int j = 0; j < lands.size() - i - 1; j++) {
-                    if (lands.get(j).calculateTax() > lands.get(j + 1).calculateTax()) {
-                        Land temp = lands.get(j);
-                        lands.set(j, lands.get(j + 1));
-                        lands.set(j + 1, temp);
+            // for (int i = 0; i < lands.size() - 1; i++) {
+            // for (int j = 0; j < lands.size() - i - 1; j++) {
+            // if (lands.get(j).calculateTax() > lands.get(j + 1).calculateTax()) {
+            // Land temp = lands.get(j);
+            // lands.set(j, lands.get(j + 1));
+            // lands.set(j + 1, temp);
+            // counter++;
+            // }
+            // }
+            // }
+
+            for (int i = 0; i < lands.length - 1; i++) {
+                for (int j = 0; j < lands.length - i - 1; j++) {
+                    if (lands[j].calculateTax() > lands[j + 1].calculateTax()) {
+                        Land temp = lands[j];
+                        lands[j] = lands[j + 1];
+                        lands[j + 1] = temp;
                         counter++;
                     }
                 }
             }
 
-
-
-            for (Land land : lands) {
-                System.out.println(land.toString());
-                System.out.printf("Tax: RM %,.2f\n", land.calculateTax());
+            for (int i = 0; i < lands.length; i++) {
+                System.out.println(lands[i].toString());
+                System.out.printf("Tax: RM %,.2f\n", lands[i].calculateTax());
                 System.out.println();
             }
+
+            // for (Land land : lands) {
+            // System.out.println(land.toString());
+            // System.out.printf("Tax: RM %,.2f\n", land.calculateTax());
+            // System.out.println();
+            // }
 
         } else if (option == 2) {
             System.out.println("Sorting using Insertion Sort\n");
 
             // sort based on id and display the list
-            for (int i = 1; i < lands.size(); i++) {
-                Land key = lands.get(i);
+            // for (int i = 1; i < lands.size(); i++) {
+            // Land key = lands.get(i);
+            // int j = i - 1;
+
+            // while (j >= 0 && lands.get(j).getId().compareTo(key.getId()) > 0) {
+            // lands.set(j + 1, lands.get(j));
+            // j--;
+            // counter++;
+            // }
+
+            // lands.set(j + 1, key);
+
+            // }
+
+            // for (Land land : lands) {
+            // System.out.println(land.toString());
+            // System.out.printf("Tax: RM %,.2f\n", land.calculateTax());
+            // System.out.println();
+            // }
+
+            for (int i = 1; i < lands.length; i++) {
+                Land key = lands[i];
                 int j = i - 1;
 
-                while (j >= 0 && lands.get(j).getId().compareTo(key.getId()) > 0) {
-                    lands.set(j + 1, lands.get(j));
+                while (j >= 0 && lands[j].getId().compareTo(key.getId()) > 0) {
+                    lands[j + 1] = lands[j];
                     j--;
                     counter++;
                 }
 
-                lands.set(j + 1, key);
-
+                lands[j + 1] = key;
             }
 
-            for (Land land : lands) {
-                System.out.println(land.toString());
-                System.out.printf("Tax: RM %,.2f\n", land.calculateTax());
+            for (int i = 0; i < lands.length; i++) {
+                System.out.println(lands[i].toString());
+                System.out.printf("Tax: RM %,.2f\n", lands[i].calculateTax());
                 System.out.println();
             }
 
         } else if (option == 3) {
             System.out.println("Searching using Binary Search\n");
             // sort based on id and display the list
-            for (int i = 1; i < lands.size(); i++) {
-                Land key = lands.get(i);
+            // for (int i = 1; i < lands.size(); i++) {
+            // Land key = lands.get(i);
+            // int j = i - 1;
+
+            // while (j >= 0 && lands.get(j).getId().compareTo(key.getId()) > 0) {
+            // lands.set(j + 1, lands.get(j));
+            // j--;
+            // }
+
+            // lands.set(j + 1, key);
+            // }
+
+            for (int i = 1; i < lands.length; i++) {
+                Land key = lands[i];
                 int j = i - 1;
 
-                while (j >= 0 && lands.get(j).getId().compareTo(key.getId()) > 0) {
-                    lands.set(j + 1, lands.get(j));
+                while (j >= 0 && lands[j].getId().compareTo(key.getId()) > 0) {
+                    lands[j + 1] = lands[j];
                     j--;
                 }
 
-                lands.set(j + 1, key);
+                lands[j + 1] = key;
             }
 
             System.out.print("Enter the ID to search: ");
             String id = strInput.nextLine();
 
             int low = 0;
-            int high = lands.size() - 1;
+            // int high = lands.size() - 1;
+            int high = lands.length - 1;
             int mid = (low + high) / 2;
 
+            // while (low <= high) {
+            // if (lands.get(mid).getId().compareTo(id) < 0) {
+            // low = mid + 1;
+            // } else if (lands.get(mid).getId().compareTo(id) == 0) {
+            // System.out.println(lands.get(mid).toString());
+            // System.out.printf("Tax: RM %,.2f\n", lands.get(mid).calculateTax());
+            // break;
+            // } else {
+            // high = mid - 1;
+            // }
+
+            // mid = (low + high) / 2;
+            // counter++;
+            // }
+
             while (low <= high) {
-                if (lands.get(mid).getId().compareTo(id) < 0) {
+                if (lands[mid].getId().compareTo(id) < 0) {
                     low = mid + 1;
-                } else if (lands.get(mid).getId().compareTo(id) == 0) {
-                    System.out.println(lands.get(mid).toString());
-                    System.out.printf("Tax: RM %,.2f\n", lands.get(mid).calculateTax());
+                } else if (lands[mid].getId().compareTo(id) == 0) {
+                    System.out.println(lands[mid].toString());
+                    System.out.printf("Tax: RM %,.2f\n", lands[mid].calculateTax());
                     break;
                 } else {
                     high = mid - 1;
