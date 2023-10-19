312675c72a5f475ad04c865e52bbfc7ab56a340d
HazeeqHaikal <haikalroslan740@gmail.com>
2023-10-19 22:07:23 +0800

    sync

--- Lab Assignment 2/q3/ArrayLists.java
+++ Lab Assignment 2/q3/ArrayLists.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class ArrayLists{
-    public ArrayLists(){
+public class ArrayLists {
+    public ArrayLists() {
 
     }
 
     // insert at back
-    public boolean add(ArrayList<Product> list, Product product){
+    public boolean add(ArrayList<Product> list, Product product) {
         return list.add(product);
     }
 
     // remove element based on object
-    public boolean remove(ArrayList<Product> list, Product product){
+    public boolean remove(ArrayList<Product> list, Product product) {
         return list.remove(product);
     }
 
     // return element from the specified location
-    public Product get(ArrayList<Product> list, int index){
+    public Product get(ArrayList<Product> list, int index) {
         return list.get(index);
     }
 
     // replace with specified element at the specified location
-    public Product set(ArrayList<Product> list, int index, Product product){
+    public Product set(ArrayList<Product> list, int index, Product product) {
         return list.set(index, product);
     }
 
     // return size of the list
-    public int size(ArrayList<Product> list){
+    public int size(ArrayList<Product> list) {
         return list.size();
     }
 
--- Lab Assignment 2/q3/Main.java
+++ Lab Assignment 2/q3/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner strInput = new Scanner(System.in);
         Scanner intInput = new Scanner(System.in);
@@ -10,16 +10,24 @@ public class Main{
         ArrayList<Product> listProduct2 = new ArrayList<Product>();
 
         // insert ten products into listProduct1
-        listProduct1.add(new Product("Apple", 1.99, 10));
-        listProduct1.add(new Product("Banana", 0.99, 20));
-        listProduct1.add(new Product("Orange", 2.99, 30));
-        listProduct1.add(new Product("Grape", 3.99, 40));
-        listProduct1.add(new Product("Watermelon", 4.99, 50));
-        listProduct1.add(new Product("Pineapple", 5.99, 60));
-        listProduct1.add(new Product("Mango", 6.99, 70));
-        listProduct1.add(new Product("Strawberry", 7.99, 80));
-        listProduct1.add(new Product("Blueberry", 8.99, 90));
         listProduct1.add(new Product("Pen", 9.99, 100));
+        listProduct1.add(new Product("Eraser", 0.99, 10000));
+        listProduct1.add(new Product("Pencil", 1.99, 300));
+        listProduct1.add(new Product("Ruler", 2.99, 259));
+        listProduct1.add(new Product("Sharpener", 3.99, 500));
+        listProduct1.add(new Product("Scissor", 4.99, 50));
+        listProduct1.add(new Product("Paper", 5.99, 99));
+        listProduct1.add(new Product("Book", 6.99, 369));
+        listProduct1.add(new Product("Bag", 7.99, 20));
+        listProduct1.add(new Product("Pencil Case", 8.99, 10));
+
+        // display all records in listProduct1
+        System.out.println("List of products in listProduct1: ");
+        for (int i = 0; i < listProduct1.size(); i++) {
+            System.out.println(listProduct1.get(i).getProductName());
+        }
+
+        System.out.println();
 
         System.out.print("Enter product name to be searched: ");
         String productName = strInput.nextLine();
@@ -27,26 +35,49 @@ public class Main{
         // search for the product
         int index = -1;
         for (int i = 0; i < listProduct1.size(); i++) {
-            if (listProduct1.get(i).getProductName().equals(productName)) {
+            if (listProduct1.get(i).getProductName().equalsIgnoreCase(productName)) {
                 index = i;
                 break;
             }
         }
 
+        System.out.println();
         if (index == -1) {
             System.out.println("Product not found!");
         } else {
             System.out.println("Product found!");
-            System.out.println("Product details: " + listProduct1.get(index));
+            System.out.println("\nProduct details:\n" + listProduct1.get(index));
         }
+
+        System.out.println();
+
+        System.out.println("Press enter to continue...");
+        strInput.nextLine();
+
+        // clear terminal
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        System.out.println("Finding product name that is pen and update quantity to 30 and price with rm1.00");
+        System.out.println();
+
+        System.out.println("Before update product details:");
+        for (int i = 0; i < listProduct1.size(); i++) {
+            if (listProduct1.get(i).getProductName().equalsIgnoreCase("pen")) {
+                System.out.println(listProduct1.get(i));
+                break;
+            }
+        }
+
+        System.out.println();
 
         // find productName that is pen and update quantity to 30 and price with rm1.00
         boolean found = false;
         for (int i = 0; i < listProduct1.size(); i++) {
-            if (listProduct1.get(i).getProductName().equals("Pen")) {
+            if (listProduct1.get(i).getProductName().equalsIgnoreCase("pen")) {
                 listProduct1.get(i).setQuantity(30);
                 listProduct1.get(i).setPrice(1.00);
-                System.out.println("Updated product details: " + listProduct1.get(i));
+                System.out.println("Updated product details:\n" + listProduct1.get(i));
                 found = true;
                 break;
             }
@@ -56,26 +87,53 @@ public class Main{
             System.out.println("Product not found!");
         }
 
-        // remove all records for total price more than rm1000 and store into listProduct2
+        System.out.println();
+
+        System.out.println("Press enter to continue...");
+        strInput.nextLine();
+
+        // clear terminal
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        System.out.println("Removing all records for total price more than rm1000 and store into listProduct2");
+
+        // remove all records for total price more than rm1000 and store into
+        // listProduct2
+        System.out.println("Removed product details:");
+        System.out.println();
         for (int i = 0; i < listProduct1.size(); i++) {
-            if (listProduct1.get(i).getPrice() * listProduct1.get(i).getQuantity() > 1000) {
+            double totalPrice = listProduct1.get(i).getPrice() * listProduct1.get(i).getQuantity();
+            if (totalPrice > 1000) {
                 listProduct2.add(listProduct1.get(i));
-                System.out.println("Removed product details: " + listProduct1.get(i));
+                System.out.println(listProduct1.get(i));
                 listProduct1.remove(i);
                 i--;
             }
         }
 
+        System.out.println();
+
+        System.out.println("Press enter to continue...");
+        strInput.nextLine();
+
+        // clear terminal
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
         // display all records in listProduct1 and listProduct2
-        System.out.println("List of products in listProduct1: ");
+        System.out.println("List of products in listProduct1:\n");
         for (int i = 0; i < listProduct1.size(); i++) {
             System.out.println(listProduct1.get(i));
         }
 
-        System.out.println("List of products in listProduct2: ");
+        System.out.println("List of products in listProduct2:\n");
         for (int i = 0; i < listProduct2.size(); i++) {
             System.out.println(listProduct2.get(i));
         }
+
+        System.out.println("Press enter to end program...");
+        strInput.nextLine();
 
     }
 }
